1d3e4062d1fec0da4d9c8c37a64c78f1df5b2337
Adam Wentz <awentz@theonion.com>
2015-02-25 21:32:32 -0600

    "World-Famous" Talking Website

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,16 @@
 'use strict';
 
-var App = require('./app');
-var app = new App();
+$(function () {
+  var meSpeak = require('mespeak');
+  meSpeak.loadConfig(require('mespeak/src/mespeak_config.json'));
+  meSpeak.loadVoice(require('mespeak/voices/en/en-us.json'));
+  
+  $('#test-form').submit(function (e) {
+    e.preventDefault();
+    var words = $('[name=words]', this).val();
+    console.log('saying ' + words);
+    meSpeak.speak(words);
+    return false;
+  });
 
-app.beep();
+});
